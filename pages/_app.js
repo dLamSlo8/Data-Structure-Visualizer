11ac1d40d4343b131b3e21fe5c01913ef851b71b
Derek Lam <dereklam67@gmail.com>
2020-11-30 02:39:34 -0800

    feat: add tailwindcss + husky for pre-commit purging

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import '../styles/main.scss';
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head' // Next-specific
 import Image from 'next/image';
 
-import styles from '../styles/Home.module.css' // Page-specific styles and assets (e.g. images)
 import BinaryTreeIcon from '../public/binary-tree.svg';
+import BinarySearchTreeIcon from '../public/binary-search-tree.svg';
 
 import DataStructureSection from '../components/pages/index/DataStructureSection'; // Page components
 import DataStructureItem from '../components/pages/index/DataStructureItem';
+
 
 export default function Home() {
   return (
@@ -14,9 +15,9 @@ export default function Home() {
         <title>Data Structure Visualizer | Home</title>
         <link rel="icon" href="/favicon.ico" />
         </Head>
-        <header>
-            <h1>Data Structure Visualizers</h1>
-            <p>This website is meant to provide interactive data structure visualizations, built with D3.js. 
+        <header className="index__header">
+            <h1 className="index__header-heading">Data Structure Visualizers</h1>
+            <p className="index__header-description">This website is meant to provide interactive data structure visualizations, built with D3.js. 
             Currently our MVP includes binary trees and binary search trees. To begin, choose a data structure from 
             the options below.</p>
         </header>
@@ -29,6 +30,10 @@ export default function Home() {
                 img={<BinaryTreeIcon />}
                 description={`Build your own binary tree using interactive controls, or 
                 enter a tree string from popular programming challenge sites like LeetCode and Binarysearch.`} />
+                <DataStructureItem
+                title="Binary Search Tree"
+                img={<BinarySearchTreeIcon />}
+                description={`Visualize a BST through adding, deleting, and searching for nodes, with animated steps.`} />
             </DataStructureSection>
         </main>
     </>
