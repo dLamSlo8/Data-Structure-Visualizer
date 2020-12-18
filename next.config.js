87dad338a49712f7830ddf15d312a1c7deccf158
Derek Lam <dereklam67@gmail.com>
2020-12-17 17:04:39 -0800

    config: update react spring to v9.0.0-rc.3

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ module.exports = {
             use: ['@svgr/webpack']
         });
 
+        /**
+         * This rule is to fix issue with react-spring v9.0.0-rc3
+         * Link: https://github.com/pmndrs/react-spring/issues/1078
+         * TO-DO: Remove when patched in v9.0.0-rc4!!!
+         */
         config.module.rules.push({
             test: /react-spring/,
             sideEffects: true
