10c4e5c72ab897e7e00050f73b7bcf77b9ec137e
unknown <maxwellwong91@gmail.com>
2020-12-27 20:12:26 -0800

    feat: added remove method and basic testing

--- classes/heap.js
+++ classes/heap.js
@@ -10,12 +10,14 @@ export default class Heap {
     /**
      * 
      * @param {TreeNode} root - root node of heap
-     * @param {function} comparator - function used to determine heap priority
+     * @param {string} [mode = null] - (optional) "min" or "max" heap
+     * @param {function} [comparator = null] - (optional) function used to determine heap priority
      */
-    constructor(root, comparator) {
+    constructor(root, mode, comparator) {
         // if going to do copy constructor or null stuff, make sure elements array match
         this.elements = [];
-        if (root !== null) {
+
+        if (root !== null && root !== undefined) {
             let idx = 0;
             this.elements = [root];
             // make sure heap is completely balanced
@@ -31,7 +33,15 @@ export default class Heap {
                 idx += 1;
             }
         }
-        this.comparator = comparator;
+        if ((mode === "min" || mode === undefined || mode === null) && (comparator === undefined || comparator === null)) {
+            this.comparator = minComparator;
+        }
+        else if (mode === "max") {
+            this.comparator = maxComparator;
+        }
+        else {
+            this.comparator = comparator;
+        }
     }
 
     /**
@@ -40,38 +50,6 @@ export default class Heap {
      * @return {TreeNode} - highest priority node
      */
     remove() {
-        /**
-         * Moves node down to maintain heap property after removal
-         * @param {int} index - index of node we are visiting
-         * @param {Array} moves - array to store moves we took
-         */
-        function bubbleDown(index, moves) {
-            if (index >= this.elements.length) {
-                return;
-            }
-
-            // leaf node so no where else to go
-            if (this.elements[index].children === null) {
-                return;
-            }
-
-            let greaterIdx = this.leftIndex(index);
-            let rightIdx = this.rightIndex(index);
-
-            if (rightIdx < this.elements.length && this.comparator(greaterIdx, rightIdx)) {
-                greaterIdx = rightIdx;
-            }
-
-            if (this.comparator(index, greaterIdx)) {
-                this.swap(index, greaterIdx, moves);
-                bubbleDown(index, moves);
-
-            } else {
-                // if curr is highest priority then don't bubble anymore
-                return;
-            }
-        }
-
         if (this.elements.length === 0) {
             throw ("Please create a tree!");
         }
@@ -84,16 +62,79 @@ export default class Heap {
         // if root has no children, only one element in heap, so root should be empty
         if (priorityNode.children === null) {
             return [priorityNode, []];
-        }
+        }        
 
         // reassign lastElement to root and set its children
         this.elements[0] = lastElement;
-        lastElement.children = priorityNode.children;
+        
+        // keep check of how many null children there are
+        let nullCount = 0;
 
-        let moves = [];
-        bubbleDown(moves);
+        // make sure original top was not parent of last element
+        for (let i = 0; i < priorityNode.children.length; i++) {
+            if (priorityNode.children[i] === lastElement) {
+                priorityNode.children[i] = NullTreeNode;
+            }
+            if (priorityNode.children[i] === NullTreeNode) {
+                nullCount += 1;
+            }
+        }
 
+        // if both children are null, then set children to null
+        lastElement.children = nullCount === 2 ? null: priorityNode.children;
+
+        // update parent of last element to be nothing
+        // get parent of length since elements has one less element because we removed,
+        // so length should have been index where last element used to be
+        let parent = this.elements[this.parentIndex(this.elements.length)];
+
+        // make sure parent no longer points to last element since last element
+        // is now root node
+        this.clearParentChildren(parent, lastElement);
+
+        let moves = [lastElement.uuid];
+        this.bubbleDown(0, moves);
+
+        // empty out children
+        priorityNode.children = null;
         return [priorityNode, moves];
+    }
+
+    /**
+     * Moves node down to maintain heap property after removal
+     * @param {int} index - index of node we are visiting
+     * @param {Array} moves - array to store moves we took
+     */
+    bubbleDown(index, moves) {
+        if (index >= this.elements.length) {
+            return;
+        }
+
+        // leaf node so no where else to go
+        if (this.elements[index].children === null) {
+            return;
+        }
+
+        let greaterIdx = this.leftIndex(index);
+        let rightIdx = this.rightIndex(index);
+
+        // make sure element exists
+        if (greaterIdx < this.elements.length) {
+            // if right side has higher priority, then need to move right
+            if (rightIdx < this.elements.length && this.comparator(this.elements[rightIdx], this.elements[greaterIdx])) {
+                greaterIdx = rightIdx;
+            }
+
+            // if higher child has greater priority, then swap
+            if (this.comparator(this.elements[greaterIdx], this.elements[index])) {
+                this.swap(index, greaterIdx, moves);
+                moves.push(this.elements[index].uuid);
+                this.bubbleDown(greaterIdx, moves);
+            } else {
+                // if curr is highest priority then don't bubble anymore
+                return;
+            }
+        }
     }
 
     /**
@@ -103,9 +144,6 @@ export default class Heap {
      */
     insert(value, uuid) {
         // need to discuss for insert if we want to show how we reached last element
-        function bubbleUp(index, moves) {
-            
-        }
         if (this.elements.length === 0) {
             throw ("Please create a tree!");
         }
@@ -123,6 +161,15 @@ export default class Heap {
     }
 
     /**
+     * Moves node up to maintain heap property after insertion
+     * @param {int} index - index of node we are visiting
+     * @param {Array} moves - array to store moves we took
+     */
+    bubbleUp(index, moves) {
+            
+    }
+
+    /**
      * Gets the highest priority element
      * @return {TreeNode} - highest priority node
      */
@@ -132,6 +179,81 @@ export default class Heap {
         }
 
         return this.elements[0];
+    }
+
+    /**
+     * Swaps two elements in heap. Updates TreeNode values and sets moves array. Element at idx1 will be
+     * child and element at idx2 will be parent of idx2.
+     * @param {int} idx1 - index of first node to swap, should be parent node that we are moving
+     * @param {int} idx2 - index of second node to swap, should be child node
+     */
+    swap(idx1, idx2) {
+        // to swap children, need to make sure that we dont have circular children dependencies
+        // bc original parent is going to be child of original child
+        let childrenForChild = this.elements[idx2].children;
+        let childrenForParent = this.elements[idx1].children;
+
+        // find child that is refering to node at idx2, change this to be
+        // node at idx1 because the node at idx1 will be child after swap
+        for (let i = 0; i < childrenForParent.length; i++) {
+            if (childrenForParent[i] === this.elements[idx2]) {
+                childrenForParent[i] = this.elements[idx1];
+            }
+        }
+        
+
+        // swap nodes
+        [this.elements[idx1], this.elements[idx2]] = [this.elements[idx2], this.elements[idx1]];
+
+        // update
+        this.elements[idx1].children = childrenForParent;
+        this.elements[idx2].children = childrenForChild;
+        
+        // update parents child
+        this.updateParentChildren(idx1);
+
+        // console.log(this.elements[idx1]);
+        // console.log(this.elements[idx2]);
+    }
+
+    /**
+     * Update parent children to be this node
+     * @param {int} idx - index of node for parent children to point to 
+     */
+    updateParentChildren(idx) {
+        let parentIdx = this.parentIndex(idx);
+        // make sure we have a parent
+        if (parentIdx >= 0) {
+            // if odd, then it should be left of parent
+            if (idx % 2 === 1) {
+                this.elements[parentIdx].setLeft(this.elements[idx]);
+            } else {
+                this.elements[parentIdx].setRight(this.elements[idx]);
+            }
+        }
+    }
+    
+    /**
+     * Set parent's child to be null
+     * @param {TreeNode} parent - parent node
+     * @param {TreeNode} child - former child of parent 
+     */
+    clearParentChildren(parent, child) {
+        if (parent.children === null) {
+            return;
+        }
+        let nullCount = 0;
+        for (let i = 0; i < parent.children.length; i++) {
+            if (parent.children[i] === child) {
+                parent.children[i] = NullTreeNode;
+            }
+            if (parent.children[i] === NullTreeNode) {
+                nullCount += 1;
+            }
+        }
+        
+        // if both children are null, then it is leaf so no children
+        parent.children = nullCount === 2 ? null: parent.children;
     }
 
     /**
@@ -160,56 +282,30 @@ export default class Heap {
     rightIndex(index) {
         return (index * 2) + 2
     }
+}
 
-    /**
-     * Swaps two elements in heap. Updates TreeNode values and sets moves array. Element at idx1 will be
-     * child and element at idx2 will be parent of idx2.
-     * @param {int} idx1 - index of first node to swap, should be node that we are moving
-     * @param {int} idx2 - index of second node to swap, should be node we are putting into moves
-     * @param {Array} moves - array of moves to keep track of
-     */
-    swap(idx1, idx2, moves) {
-        moves.push(this.elements[idx2].uuid);
-
-        // swap children, need to make sure that we dont have circular children dependencies
-        // bc original parent is going to be child of original child
-        let childrenForChild = this.elements[idx2].children;
-        let childrenForParent = this.elements[idx1].children;
-
-        // find child that is refering to node at idx2, change this to be
-        // node at idx1 because the node at idx1 will be child after swap
-        for (let i = 0; i < childrenForParent.length; i++) {
-            if (childrenForParent[i] === this.elements[idx2]) {
-                childrenForParent[i] = this.elements[idx1];
-            }
-        }
-        
-
-        // swap nodes
-        [this.elements[idx1], this.elements[idx2]] = [this.elements[idx2], this.elements[idx1]];
-
-        // update
-        this.elements[idx1].children = childrenForParent;
-        this.elements[idx2].children = childrenForChild;
-        
-        // update parents child
-        this.updateParentChildren(idx1);
+/**
+ * Determines priority between two nodes. Higher priority for smaller node
+ * @param {TreeNode} node1 - first node to compare
+ * @param {TreeNode} node2 - second node to compare
+ * @return {boolean} - whether node1 has higher priority than node2 
+ */
+export function minComparator(node1, node2) {
+    if (node1.name < node2.name) {
+        return true;
     }
+    return false;
+}
 
-    /**
-     * Update parent children to be this node
-     * @param {int} idx - index of node for parent children to point to 
-     */
-    updateParentChildren(idx) {
-        let parentIdx = this.parentIndex(idx);
-        // make sure we have a parent
-        if (parentIdx >= 0) {
-            // if odd, then it should be left of parent
-            if (idx % 2 === 1) {
-                this.elements[parentIdx].setLeft(this.elements[idx]);
-            } else {
-                this.elements[parentIdx].setRight(this.elements[idx]);
-            }
-        }
+/**
+ * Determines priority between two nodes. Higher priority for larger node
+ * @param {TreeNode} node1 - first node to compare
+ * @param {TreeNode} node2 - second node to compare
+ * @return {boolean} - whether node1 has higher priority than node2 
+ */
+export function maxComparator(node1, node2) {
+    if (node1.name > node2.name) {
+        return true;
     }
+    return false;
 }
--- test/classes/heap.test.js
+++ test/classes/heap.test.js
@@ -1,6 +1,7 @@
 import TreeNode, { NullTreeNode } from "../../classes/tree-node.js";
 import Heap from "../../classes/heap.js";
 
+
 describe ("Test constructor", () => {
     it ("Should fill up elements array on creation", () => {
         let root = new TreeNode(1, "a");
@@ -171,5 +172,172 @@ describe ("Test swap method", () => {
 
         result.swap(2, 6, []);
         expect(result.elements).toEqual(expectedElements);
+    })
+})
+
+describe ("Test clearParentChild method", () => {
+    it ("Should remove child from parent", () => {
+        // test remove left
+        let root = new TreeNode(11, "a");
+        root.setLeft(new TreeNode(7, "b"));
+
+        let input = new Heap(root);
+
+        let expected = new TreeNode(11, "a");
+
+        input.clearParentChildren(root, root.children[0]);
+        expect(input.elements[0]).toMatchObject(expected);
+
+        // test remove right
+        root = new TreeNode(11, "a");
+        root.setLeft(new TreeNode(7, "b"));
+        root.setRight(new TreeNode(10, "c"));
+
+        input = new Heap(root);
+
+        expected = new TreeNode(11, "a");
+        expected.setLeft(new TreeNode(7, "b"))
+
+        input.clearParentChildren(root, root.children[1]);
+        expect(input.elements[0]).toMatchObject(expected);
+    })
+})
+
+describe ("Test top method", () => {
+    it ("Should throw an error when empty tree", () => {
+        let result = new Heap();
+
+        expect(() => result.top()).toThrow("Please create a tree!");
+    })
+
+    it ("Should return highest priority node", () => {
+        let root = new TreeNode(10, "a");
+        root.setLeft = new TreeNode(4, "b");
+        root.setRight = new TreeNode(8, "c");
+
+        let input = new Heap(root);
+
+        let expected = new TreeNode(10, "a");
+        expected.setLeft = new TreeNode(4, "b");
+        expected.setRight = new TreeNode(8, "c");
+
+        let result = input.top();
+
+        expect(result).toMatchObject(expected);
+    })
+})
+
+describe ("Test remove method", () => {
+    it ("Should throw an error when empty tree", () => {
+        let result = new Heap();
+
+        expect(() => result.remove()).toThrow("Please create a tree!");
+    })
+
+    it ("Should set tree and update moves array after remove", () => {
+        // discuss moves array
+        // delete one element in tree
+        let root = new TreeNode(10, "a");
+
+        let input = new Heap(root, "max");
+
+        let expectedNode = new TreeNode(10, "a"); 
+        let expectedMoves = [];
+        let expectedElements = [];
+
+        let result = input.remove();
+
+        expect(result[0]).toMatchObject(expectedNode);
+        expect(result[1]).toEqual(expectedMoves);
+        expect(input.elements).toEqual(expectedElements);
+
+        // delete no bubble
+        root = new TreeNode(10, "a");
+        root.setLeft(new TreeNode(12, "b"));
+
+        input = new Heap(root);
+
+        let expected = new TreeNode(10, "a");
+
+        expectedNode = new TreeNode(12, "b"); 
+        expectedMoves = ["b"];
+        expectedElements = [expectedNode];
+
+        result = input.remove();
+
+        expect(result[0]).toMatchObject(expected);
+        expect(result[1]).toEqual(expectedMoves);
+        expect(input.elements).toEqual(expectedElements);
+
+        root = new TreeNode(10, "a");
+        root.setLeft(new TreeNode(12, "b"));
+        root.children[0].setLeft(new TreeNode(18, "c"));
+        root.children[0].setRight(new TreeNode(20, "d"));
+        root.setRight(new TreeNode(15, "e"));
+
+        input = new Heap(root);
+
+        expected = new TreeNode(10, "a");
+
+        expectedNode = new TreeNode(12, "b");
+        expectedNode.setLeft(new TreeNode(18, "c"));
+        expectedNode.children[0].setLeft(new TreeNode(20, "d"));
+        expectedNode.setRight(new TreeNode(15, "e"));
+
+        expectedMoves = ["d", "b", "c"];
+        expectedElements = [
+            expectedNode,
+            expectedNode.children[0], expectedNode.children[1],
+            expectedNode.children[0].children[0]
+        ];
+
+        result = input.remove();
+ 
+        expect(result[0]).toMatchObject(expected);
+        expect(result[1]).toEqual(expectedMoves);
+        expect(input.elements).toEqual(expectedElements);
+
+
+        // delete and bubble down left
+        root = new TreeNode(10, "a");
+        root.setLeft(new TreeNode(7, "b"));
+        root.children[0].setLeft(new TreeNode(5, "c"));
+        root.children[0].children[0].setLeft(new TreeNode(2, "d"));
+        root.children[0].children[0].setRight(new TreeNode(1, "e"));
+        root.children[0].setRight(new TreeNode(6, "f"));
+        root.children[0].children[1].setLeft(new TreeNode(3, "g"));
+        root.children[0].children[1].setRight(new TreeNode(-4, "h"));
+        root.setRight(new TreeNode(4, "i"));
+        root.children[1].setLeft(new TreeNode(2, "j"));
+        root.children[1].setRight(new TreeNode(-1, "k"));
+
+        input = new Heap(root, "max");
+
+        expected = new TreeNode(10, "a");
+
+        expectedNode = new TreeNode(7, "b");
+        expectedNode.setLeft(new TreeNode(6, "f"));
+        expectedNode.children[0].setLeft(new TreeNode(5, "c"));
+        expectedNode.children[0].children[0].setLeft(new TreeNode(2, "d"));
+        expectedNode.children[0].children[0].setRight(new TreeNode(1, "e"));
+        expectedNode.children[0].setRight(new TreeNode(3, "g"));
+        expectedNode.children[0].children[1].setLeft(new TreeNode(-4, "h"));
+        expectedNode.setRight(new TreeNode(4, "i"));
+        expectedNode.children[1].setLeft(new TreeNode(2, "j"));
+        expectedNode.children[1].setRight(new TreeNode(-1, "k"));
+
+        result = input.remove();
+
+        expectedMoves = ["h", "b", "f", "g"];
+        expectedElements = [
+            expectedNode,
+            expectedNode.children[0], expectedNode.children[1],
+            expectedNode.children[0].children[0], expectedNode.children[0].children[1], expectedNode.children[1].children[0], expectedNode.children[1].children[1],
+            expectedNode.children[0].children[0].children[0], expectedNode.children[0].children[0].children[1], expectedNode.children[0].children[1].children[0]
+        ];
+
+        expect(result[0]).toMatchObject(expected);
+        expect(result[1]).toEqual(expectedMoves);
+        expect(input.elements).toEqual(expectedElements);
     })
 })
